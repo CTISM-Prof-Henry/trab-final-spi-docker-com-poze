2b92b7fd47ab583baa751822e9fd2a4f783e8cd5
João Guedes <joaoguedes1404@gmail.com>
2025-08-22 09:54:31 -0300

    Adiciona frontend como parte do repositório principal

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function About() {
+    return (
+        <div>
+            <h1>Router test</h1>
+            <Button variant="secondary">This is a test button</Button>
+        </div>
+    );
+}
