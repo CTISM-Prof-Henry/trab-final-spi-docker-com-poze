8e64e2baf1e7a573d931cc5597113ad63d65e9b4
João Guedes <joaoguedes1404@gmail.com>
2025-08-31 23:33:11 -0300

    Adicionando middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,55 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = [
+    {
+        path: '/login',
+        whenAuthenticated: 'redirect'
+    },
+    {
+        path: '/register',
+        whenAuthenticated: 'redirect'
+    }
+] as const;
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED = '/login';
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+    const publicRoute = publicRoutes.find(route => route.path === path);
+    const authToken = request.cookies.get('authToken');
+
+    if (!authToken && publicRoute) {
+        return NextResponse.next();
+    }
+
+    if (!authToken && !publicRoute) {
+        const url = request.nextUrl.clone();
+        url.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED;
+        return NextResponse.redirect(url);
+    }
+
+    if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
+        const url = request.nextUrl.clone();
+        url.pathname = '/';
+        return NextResponse.redirect(url);
+    }
+
+    if (authToken && !publicRoute) {
+        return NextResponse.next();
+    }
+
+    return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+    matcher: [
+        /*
+        * Match all request paths except for the ones starting with:
+        * - api (API routes)
+        * - _next/static (static files)
+        * - _next/image (image optimization files)
+        * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+        */
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    ],
+};
