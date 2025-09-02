1f4d7917813947c179f9f2110f56fd8b6e1f3ffe
João Guedes <joaoguedes1404@gmail.com>
2025-09-01 23:07:03 -0300

    Brincando com as rotas

--- frontend/src/app/(public)/login/page.tsx
+++ frontend/src/app/(public)/login/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
+import Link from "next/link";
 
 export default function Login() {
+    function handleMaskedInput(event: React.ChangeEvent<HTMLInputElement>) {
+        let value = event.target.value;
+        value = value.replace(/\D/g, '');
+        event.target.value = value;
+    }
+    
     return (
         <div className="flex min-h-screen items-center justify-center p-4 min-w-screen">
             <Card className="bg-neutral-800 w-xl min-w-72">
@@ -11,18 +20,20 @@ export default function Login() {
                     <CardTitle>Login page</CardTitle>
                     <CardDescription>Faça login na nossa plataforma!</CardDescription>
                     <CardAction>
-                        <Button variant="link">Cadastrar</Button>
+                        <Button variant="link">
+                            <Link href="/register">Cadastrar</Link>
+                        </Button>
                     </CardAction>
                 </CardHeader>
                 <CardContent>
                     <form>
                         <div className="flex flex-col gap-6">
                             <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
+                                <Label htmlFor="email">Matricula</Label>
                                 <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="email@email.com"
+                                    id="matricula"
+                                    type="text"
+                                    onChange={handleMaskedInput}
                                     required
                                 />
                             </div>
@@ -32,6 +43,9 @@ export default function Login() {
                                 </div>
                                 <Input id="password" type="password" required />
                             </div>
+                        </div>
+                        <div className="mt-6 flex justify-center">
+                            <Button type="submit" className="w-full" variant="outline">Login</Button>
                         </div>
                     </form>
                 </CardContent>
---
+++ frontend/src/app/(public)/register/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Register() {
+    return (
+        <div>
+            <h1 className="text-3xl text-black">Pagina de registro.</h1>
+        </div>
+    );
+}
