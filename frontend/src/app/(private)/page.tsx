c22f0477f8174044767d8eb2dbac680496650173
João Guedes <joaoguedes1404@gmail.com>
2025-09-08 23:15:54 -0300

    style: Adicionando comentarios.

--- frontend/src/app/(private)/page.tsx
+++ frontend/src/app/(private)/page.tsx
@@ -1,4 +1,10 @@
 export default function Home() {
+
+  // Minha ideia aqui é pegar o dado do usuário e ver
+  // Se eh admin ou professor/aluno
+  // Pois dai poderia redirecionar para o portal.
+  // A home no fim seria so um fallback.
+
   return (
     <div>
     </div>
