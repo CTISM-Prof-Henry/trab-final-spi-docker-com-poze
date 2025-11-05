0398f21760765428cb5ed15a9e3b175780e8c885
PDSA <pdsa@Glenios-MacBook-Pro.local>
2025-11-04 22:16:49 -0300

    feat: iniciando tela de listagem de salas.

--- frontend/src/app/(private)/portal-admin/salas/page.tsx
+++ frontend/src/app/(private)/portal-admin/salas/page.tsx
@@ -1,4 +1,31 @@
+'use client';
+
+import { SalaDTO } from "@/app/core/dto/sala.dto";
+import api from "@/lib/api";
+import { useEffect, useState } from "react";
+
 export default function AdminSalas() {
+
+    const [salas, setSalas] = useState<SalaDTO[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [page, setPage] = useState<number>(0);
+    const [limit, setLimit] = useState<number>(20);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        const fetchSalasPaginated = async () => {
+            try {
+                const response = await api.get(`/sala/find-all/paginated/${page}/${limit}`);
+                console.log(response);
+            } catch(error) {
+                console.error("Erro ao buscar salas: " + error)
+            }
+        }
+
+        fetchSalasPaginated();
+    }, []);
+
     return (
         <div>
             <div>
