110b8448e808c75da05fe693fd2341f3942d100f
Gue10 <joaoguedes1404@gmail.com>
2025-11-09 19:19:48 -0300

    feat - Iniciando pagina de agendamentos.

--- frontend/src/app/(private)/portal-admin/agendamentos/page.tsx
+++ frontend/src/app/(private)/portal-admin/agendamentos/page.tsx
@@ -1,7 +1,35 @@
+'use client';
+
+import { AgendamentoDTO } from "@/app/core/dto/agendamento.dto";
+import api from "@/lib/api";
+import { useEffect, useState } from "react";
+
 export default function AgendamentosAdmin() {
+    
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [agendamentos, setAgendamentos] = useState<AgendamentoDTO[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [editingAgendamento, setEditingAgendamento] = useState<boolean>(false);
+
+    async function fetchAgendamentos() {
+        setIsLoading(true);
+        try {
+            //const response = await api.get<AgendamentoDTO[]>('/agendamentos');
+            //setAgendamentos(response.data);
+        } catch(error) {
+            console.error("Erro ao buscar agendamentos:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchAgendamentos();
+    }, []);
+
     return (
         <div>
             <div>Agendamentos Admin - Em andamento...</div>
-        </div>  
+        </div>
     );
 }
